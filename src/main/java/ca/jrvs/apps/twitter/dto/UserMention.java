9953f31f1d35e7cf7535dbd15c3070342d42b3b9
SiddhantSatam <51959242+SiddhantSatam@users.noreply.github.com>
2019-07-21 21:22:28 -0400

    Final commit

--- src/main/java/ca/jrvs/apps/twitter/dto/UserMention.java
+++ src/main/java/ca/jrvs/apps/twitter/dto/UserMention.java
@@ -3,7 +3,6 @@ package ca.jrvs.apps.twitter.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
