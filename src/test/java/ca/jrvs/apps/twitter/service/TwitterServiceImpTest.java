db294f88ac242fe388f598f070a3e66d3f98d134
SiddhantSatam <51959242+SiddhantSatam@users.noreply.github.com>
2019-07-21 21:51:04 -0400

    Final commit

---
+++ src/test/java/ca/jrvs/apps/twitter/service/TwitterServiceImpTest.java
@@ -0,0 +1,29 @@
+package ca.jrvs.apps.twitter.Service;
+
+import ca.jrvs.apps.twitter.dao.CrdRepo;
+import ca.jrvs.apps.twitter.dto.Tweet;
+import org.junit.Test;
+import org.mockito.Mockito;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class TwitterServiceImpTest {
+
+    @Test
+    public void postTweet() {
+        CrdRepo Mockdao = Mockito.mock(CrdRepo.class);
+        TwitterService service = new TwitterServiceImp(Mockdao);
+        Tweet mockTweet = new Tweet();
+        mockTweet.setText("Mockito Tweet");
+        when(Mockdao.create(any())).thenReturn(mockTweet);
+        service.postTweet("Random Tweet", 0.0, 0.0);
+
+        try {
+            service.postTweet("Another Tweet", 50.0, 390.0);
+            fail();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+}
