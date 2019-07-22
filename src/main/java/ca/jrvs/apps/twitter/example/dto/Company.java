1306b4b43f987371f511a091bc16709ad7d5bc74
SiddhantSatam <51959242+SiddhantSatam@users.noreply.github.com>
2019-07-21 21:12:51 -0400

    Final commit with code cleaning

--- src/main/java/ca/jrvs/apps/twitter/example/dto/Company.java
+++ src/main/java/ca/jrvs/apps/twitter/example/dto/Company.java
@@ -2,10 +2,6 @@ package ca.jrvs.apps.twitter.example.dto;
 
 import com.fasterxml.jackson.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
         "symbol",
